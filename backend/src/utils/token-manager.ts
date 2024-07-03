4906627a848d13dfa979bd837de5385c43066fe4
Ismael <ismael.seara@gmail.com>
2024-07-03 15:08:58 -0300

    added jasonwebtoken

--- backend/src/utils/token-manager.ts
+++ backend/src/utils/token-manager.ts
@@ -1,1 +1,7 @@
 import crypto from 'crypto';
+import jwt from 'jsonwebtoken';
+
+const createToken = (id: string, email: string, exiresIn) {
+  const payload = {id, email};
+  const toket = jwt.sign
+}
